9eb54821850fbae10f586c1c431a0982364c3220
Anshu <anshu.possible@gmail.com>
2024-02-13 15:04:31 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",
-{id: "parent"},
-React.createElement(
-    "div",
-    {id: "child"},
-    React.createElement("h1", {}, "I am h1 tag")
-    )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "by Anshu"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 
-console.log(parent);
-
-/* const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello From React !!"
-);
- */
+console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
 
 root.render(parent);
